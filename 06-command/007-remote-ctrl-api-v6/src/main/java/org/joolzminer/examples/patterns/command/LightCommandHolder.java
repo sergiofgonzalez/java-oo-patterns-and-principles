2c7192c40d018d498b2d2cc1f8ae702a52e790fd
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2016-12-19 14:21:14 +0100

    Fix incorrent implementation used for testing

--- 06-command/007-remote-ctrl-api-v6/src/main/java/org/joolzminer/examples/patterns/command/LightCommandHolder.java
+++ 06-command/007-remote-ctrl-api-v6/src/main/java/org/joolzminer/examples/patterns/command/LightCommandHolder.java
@@ -6,20 +6,17 @@ public class LightCommandHolder {
 	
 	private Light light; 
 	
-	private Command lightOnCommand = () -> { 
-		System.out.println("inside"); 
-		light.on(); };
+	private Command lightOnCommand = () -> light.on();
 	
 	private Command lightOffCommand = () -> light.off();
 	
 	public LightCommandHolder(Light light) {
 		this.light = light;
 	}	
-		
+	
+	
 	public Command getLightOnCommand() {
-		return () -> { 
-			System.out.println("inside"); 
-			light.on(); };
+		return lightOnCommand;
 	}
 	
 	public Command getLightOffCommand() {
